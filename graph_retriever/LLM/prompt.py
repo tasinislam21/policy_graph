996a9404e920249dd106f85a69a5fed4ef1cf49b
Dev <tasin17@hotmail.com>
2025-07-15 13:15:58 +0100

    fixed prompt

--- graph_retriever/LLM/prompt.py
+++ graph_retriever/LLM/prompt.py
@@ -11,18 +11,16 @@ Note: Do not include any explanations or apologies in your responses.
 Do not include any text except the generated Cypher statement. Remember to correct the typo in names
 
 Example 1: What was the story of napoleon in the battle of waterloo?
-MATCH (Napoleon:Person)-[:RELATED_TO]->(waterloo:Event)-[:HAS_SECTION]->(info:Section)-[:HAS_Chunk]->(ChunkInfo:Chunk)
-RETURN p, e, info, ChunkInfo.text
+MATCH (Napoleon:Person {name: "Napoleon"})-[:RELATED_TO]->(waterloo:Event)-[:HAS_SECTION]->(info:Section)-[:HAS_CHUNK]->(ChunkInfo:Chunk)
+RETURN ChunkInfo.text
 
 Example 2: What was the story of the battle of waterloo?
-MATCH (waterloo:Event)-[:HAS_SECTION]->(info:Section '{{type: "General Information"}}')-[:HAS_Chunk]->(ChunkInfo:Chunk)
-RETURN p, e, info, ChunkInfo.text
+MATCH (waterloo:Event)-[:HAS_SECTION]->(info:Section {type: "General information"})-[:HAS_CHUNK]->(ChunkInfo:Chunk)
+RETURN ChunkInfo.text
 
 Example 3: tell me about Talleyrand and napoleon in 5 lines
-MATCH (Talleyrand:Person)-[:RELATED_TO]->(Napoleon:Person)-[:HAS_SECTION]->(info:Section '{{type: "Career"}}')-[:HAS_Chunk]->(ChunkInfo:Chunk)
-RETURN Talleyrand, Napoleon
-
-
+MATCH (Talleyrand:Person)-[:RELATED_TO]->(Napoleon:Person)-[:HAS_SECTION]->(info:Section {type: "Career"})-[:HAS_CHUNK]->(ChunkInfo:Chunk)
+RETURN ChunkInfo.text
 
 Help:
 This is all existing property for Node Person and Section:
@@ -66,12 +64,8 @@ Node Section Properties:
 
     parent_name: "Napoleon"
     type: "General Information"
-    
-
-
-
 
 The question is:
 {question}
- 
+
 """ 
